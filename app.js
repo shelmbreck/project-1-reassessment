24dc7abd33d3871f47fda013c56144eb82a0d882
shelmbreck <samantha.helmbreck@gmail.com>
2019-04-11 10:44:06 -0700

    final commit

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+fDegrees = 32
+cDegrees = '0.00C'
+
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('convert').addEventListener('click', function(e) {
-        // e.preventDefault();
+    document.getElementById('convert').addEventListener('click', function() {
         convert()
     });
 })
 
 function convert() {
+    document.getElementById('temp').textContent = fDegrees
+    document.getElementById('result').textContent = "Results: " + cDegrees
     var fahrenheit = document.getElementById('temp').value;
-    var celsius = (fahrenheit - 32) * (5/9);
-    document.getElementById('display').textContent = celsius;
+    var celsius = ((fahrenheit - 32) * (5/9)).toFixed(2)
+    document.getElementById('result').textContent = celsius;
 }
